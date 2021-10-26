e1a0ed907a36dee2e4f854fbd492a57c26eae7f4
korolev solutions <neoevil25@gmail.com>
2021-10-26 20:12:07 +0300

    point2: settings view added

--- amkorolevPW2/ViewController.swift
+++ amkorolevPW2/ViewController.swift
@@ -8,10 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let settingsView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.lightGray
+        setupSettingsView()
         setupSettingsButton()
     }
 
@@ -22,17 +25,33 @@ class ViewController: UIViewController {
     private func setupSettingsButton() {
         let settingsButton = UIButton(type: .system)
         view.addSubview(settingsButton)
-        print("btn pushed")
+        print("btn created")
         //settingsButton.setTitle("settings", for: .normal)
         settingsButton.setImage(UIImage(named: "settings")?.withRenderingMode(.alwaysOriginal), for: .normal)
         
-        
+        settingsButton.addTarget(self, action: #selector(settingsButtonPressed), for: .touchUpInside)
         
         settingsButton.translatesAutoresizingMaskIntoConstraints = false
         settingsButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15).isActive = true
         settingsButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -15).isActive = true
         settingsButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
         settingsButton.widthAnchor.constraint(equalTo: settingsButton.heightAnchor).isActive = true
+    }
+    
+    @objc private func settingsButtonPressed() {
+        UIView.animate(withDuration: 0.2, animations: {self.settingsView.alpha = 1 - self.settingsView.alpha})
+    }
+    
+    private func setupSettingsView() {
+        view.addSubview(settingsView)
+        settingsView.backgroundColor = UIColor.darkGray
+        settingsView.alpha = 0
+        
+        settingsView.translatesAutoresizingMaskIntoConstraints = false
+        settingsView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
+        settingsView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10).isActive = true
+        settingsView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        settingsView.widthAnchor.constraint(equalToConstant: 200).isActive = true
     }
     
 }
